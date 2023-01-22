cfbb8f69ff71bb2bd045be86e63c896ed54958ae
fullstacknewb <bjornbartley@gmail.com>
2023-01-22 15:15:46 -0500

    first commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,20 @@
 import '@/styles/globals.css'
+import Navbar from '@/components/Navbar'
+
+import { UserContext } from '@/lib/context'
+import { Toaster } from 'react-hot-toast'
+import { useUserData } from '@/lib/hooks'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const userData = useUserData()
+  return (
+    <>
+     <UserContext.Provider value={userData}>
+      <Navbar />
+      <Component {...pageProps} />
+      <Toaster />
+    </UserContext.Provider>
+    </>
+  )
+  
 }
